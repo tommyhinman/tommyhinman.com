7c3e45edc022e9941e0982ef74f44c5587672622
tommyhinman <tommyhinman@gmail.com>
2015-11-26 18:55:38 -0800

    Added year to URL for linking to individual lists.

--- js/albumLists.js
+++ js/albumLists.js
@@ -3,9 +3,11 @@ angular.module('albumLists', ['ngAnimate', 'ui.bootstrap'])
 	  $interpolateProvider.startSymbol('{[');
 	  $interpolateProvider.endSymbol(']}');
 	}])
-	.controller('albumListsController', function ($scope, $log) {
+	.controller('albumListsController', function ($scope, $log, $location) {
 		$scope.options = ['2011','2012','2013','2014']
-		$scope.selectedYear = '2014';
+		if($location.path()) {
+			$scope.selectedYear = $location.path().replace('/','');
+		}
 
 		$scope.status = {
 			isopen: false
@@ -13,10 +15,12 @@ angular.module('albumLists', ['ngAnimate', 'ui.bootstrap'])
 
 		$scope.toggled = function(open) {
 			$log.log('Dropdown is now ', open);
+
 		};
 
 		$scope.selected = function(item) {
-			$scope.selectedYear = item
+			$scope.selectedYear = item;
+			$location.path(item);
 		}
 
 	});
